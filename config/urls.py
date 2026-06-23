d9b390adaa47a73a51c9b8c0e6b71da9163c865e
MuahammadKarim Islomov <muhammadkarim@gmail.com>
2026-06-23 01:48:32 -0500

    Fix: serve media files in production

--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Media fayllar - har doim serve qilish
+from django.views.static import serve
+from django.urls import re_path
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
